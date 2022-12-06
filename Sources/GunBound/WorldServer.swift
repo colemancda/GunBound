27f7912282965372962682d797b709b2dbe03a66
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-06 07:53:21 -0800

    Add `WorldServer`

---
+++ Sources/GunBound/WorldServer.swift
@@ -0,0 +1,40 @@
+//
+//  WorldServer.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/6/22.
+//
+
+import Foundation
+import Socket
+
+public actor WorldServer {
+        
+    internal static let encoder = GunBoundEncoder()
+    
+    public init() {
+        
+    }
+    
+    public func handle(address: IPv4Address, packet: Packet) async -> Packet {
+        do {
+            switch packet.command {
+            case .authenticationRequest:
+                return try authenticate(packet)
+            default:
+                fatalError()
+            }
+        } catch {
+            fatalError()
+        }
+    }
+    
+    func errorResponse(_ error: Error) -> Packet {
+        fatalError()
+    }
+    
+    func authenticate(_ packet: Packet) throws -> Packet {
+        //return try Self.encoder.encode(ServerDirectoryResponse(directory: directory))
+        fatalError()
+    }
+}
