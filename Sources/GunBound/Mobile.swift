ea7392c26f455006404a8ab365fb48d49eb6c7ee
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-06 07:52:35 -0800

    Add `Mobile`

---
+++ Sources/GunBound/Mobile.swift
@@ -0,0 +1,28 @@
+//
+//  Mobile.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/6/22.
+//
+
+/// GunBound Mobile / Tanks
+public enum Mobile: UInt8 {
+    
+    case armor = 0x00
+    case mage = 0x01
+    case nak = 0x02
+    case trico = 0x03
+    case bigfoot = 0x04
+    case boomer = 0x05
+    case raon = 0x06
+    case lightning = 0x07
+    case jd = 0x08
+    case asate = 0x09
+    case ice = 0x0A
+    case turtle = 0x0B
+    case grub = 0x0C
+    case aduka = 0x0D
+    case dragon = 0x11
+    case knight = 0x12
+    case random = 0xFF
+}
