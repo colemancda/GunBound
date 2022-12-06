771f8ec7c82d85ca55dbf36a3d0ef3b154ca0545
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-06 00:37:45 -0800

    Add `GunBoundServer.Connection`

--- Sources/GunBound/Server.swift
+++ Sources/GunBound/Server.swift
@@ -2,11 +2,25 @@ import Foundation
 import Socket
 
 /// GunBound Classic Server
-public final class GunBoundServer {
+public actor GunBoundServer {
     
     public let address: IPv4SocketAddress
     
     public init(address: IPv4SocketAddress) {
         self.address = address
+    }
+    
+    
+}
+
+internal extension GunBoundServer {
+    
+    actor Connection {
+        
+        let address: IPv4SocketAddress
+        
+        public init(address: IPv4SocketAddress) {
+            self.address = address
+        }
     }
 }
