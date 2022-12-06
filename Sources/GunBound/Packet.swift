5df55a010d28532918d132e2432806ecc4203492
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-06 00:38:18 -0800

    Add `Packet.data`

--- Sources/GunBound/Packet.swift
+++ Sources/GunBound/Packet.swift
@@ -7,7 +7,61 @@
 
 import Foundation
 
+/// GunBound Packet
 public struct Packet {
+    
+    public let data: Data
+    
+    public init?(data: Data) {
+        // validate size
+        guard data.count >= Packet.minSize,
+              data.count <= Packet.maxSize else {
+            return nil
+        }
+        self.data = data
+    }
+}
+
+public extension Packet {
+    
+    static var minSize: Int { 6 }
+    
+    static var maxSize: Int { 1024 }
+}
+/*
+public extension Packet {
+    
+    /// Packet size
+    var size: UInt16 {
+        UInt16(parameters.count)
+    }
+    
+    /// Packet sequence
+    var sequence: UInt16 {
+        
+    }
+    
+    /// Packet command
+    var command: Command {
+        
+    }
+    
+    /// Packet parameters
+    var parameters: Data {
+        withParameters { Data($0) }
+    }
+    
+    func withParameters<ResultType>(_ body: (UnsafeRawBufferPointer) throws -> ResultType) rethrows -> ResultType {
+        try data.withUnsafeBytes { pointer in
+            try body(pointer.advanced(by: 6))
+        }
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension Packet: CustomStringConvertible {
     
     
 }
+*/
