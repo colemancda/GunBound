7dadb61a540134798bffe6931d1ae925cf0871e1
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-06 07:53:10 -0800

    Add `Rank`

---
+++ Sources/GunBound/Rank.swift
@@ -0,0 +1,36 @@
+//
+//  Rank.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/6/22.
+//
+
+/// GunBound Rank
+public enum Rank: Int16 {
+    
+    case administrator = 20
+    case chick = 19
+    case wooden = 18
+    case doubleWooden = 17
+    case stone = 16
+    case doubleStone = 15
+    case axe = 14
+    case doubleAxe = 13
+    case silverAxe = 12
+    case doubleSilverAxe = 11
+    case goldAxe = 10
+    case doubleGoldAxe = 9
+    case battleAxe = 8
+    case battleAxePlus = 7
+    case silverBattleAxe = 6
+    case silverBattleAxePlus = 5
+    case goldenBattleAxe = 4
+    case goldenBattleAxePlus = 3
+    case violetWand = 2
+    case sapphireWand = 1
+    case rubyWand = 0
+    case diamondWand = -1
+    case blueDragon = -2
+    case redDragon = -3
+    case silverDragon = -4
+}
