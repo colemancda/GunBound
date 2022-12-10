e27fa793c2b1d880a1fee029941025e9951b02a5
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-09 23:04:42 -0800

    Add `Codable` conformance to `Mobile`

--- Sources/GunBound/Mobile.swift
+++ Sources/GunBound/Mobile.swift
@@ -6,23 +6,23 @@
 //
 
 /// GunBound Mobile / Tanks
-public enum Mobile: UInt8 {
+public enum Mobile: UInt8, Codable {
     
-    case armor = 0x00
-    case mage = 0x01
-    case nak = 0x02
-    case trico = 0x03
-    case bigfoot = 0x04
-    case boomer = 0x05
-    case raon = 0x06
-    case lightning = 0x07
-    case jd = 0x08
-    case asate = 0x09
-    case ice = 0x0A
-    case turtle = 0x0B
-    case grub = 0x0C
-    case aduka = 0x0D
-    case dragon = 0x11
-    case knight = 0x12
-    case random = 0xFF
+    case armor      = 0x00
+    case mage       = 0x01
+    case nak        = 0x02
+    case trico      = 0x03
+    case bigfoot    = 0x04
+    case boomer     = 0x05
+    case raon       = 0x06
+    case lightning  = 0x07
+    case jd         = 0x08
+    case asate      = 0x09
+    case ice        = 0x0A
+    case turtle     = 0x0B
+    case grub       = 0x0C
+    case aduka      = 0x0D
+    case dragon     = 0x11
+    case knight     = 0x12
+    case random     = 0xFF
 }
