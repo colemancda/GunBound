2dda61b9f2666f1b86de9dbdeb781fb7174a0eb8
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-09 16:48:48 -0800

    Add `JoinChannelRequest`

---
+++ Sources/GunBound/Packets/JoinChannelRequest.swift
@@ -0,0 +1,16 @@
+//
+//  JoinChannelRequest.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/9/22.
+//
+
+import Foundation
+
+/// GunBound Join Channel Request packet
+public struct JoinChannelRequest: GunBoundPacket, Equatable, Hashable, Codable {
+    
+    public static var opcode: Opcode { .joinChannelRequest }
+        
+    public let channel: Channel
+}
