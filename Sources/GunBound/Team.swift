b50f37ec33163144d786add50e1a251a505447a1
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-10 12:42:08 -0800

    Add `Team.description`

--- Sources/GunBound/Team.swift
+++ Sources/GunBound/Team.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  Team.swift
 //  
 //
 //  Created by Alsey Coleman Miller on 12/9/22.
@@ -7,11 +7,29 @@
 
 import Foundation
 
-public enum Team: UInt8, Codable {
+public enum Team: UInt8, Codable, CaseIterable {
     
     /// Team A
     case a = 0x00
     
     /// Team B
     case b = 0x01
+}
+
+// MARK: - CustomStringConvertible
+
+extension Team: CustomStringConvertible, CustomDebugStringConvertible {
+    
+    public var description: String {
+        switch self {
+        case .a:
+            return "A"
+        case .b:
+            return "B"
+        }
+    }
+    
+    public var debugDescription: String {
+        description
+    }
 }
