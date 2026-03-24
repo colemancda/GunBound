fe76208fa07430c04361bb511db6da1e0964818f
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-23 21:38:32 -0400

    Add SetAvatarResponse (0x6005)

---
+++ Sources/GunBound/Packets/SetAvatarResponse.swift
@@ -0,0 +1,11 @@
+/// Set Avatar Response (0x6005)
+public struct SetAvatarResponse: GunBoundPacket, GunBoundEncodable, Encodable {
+
+    public static var opcode: Opcode { .setAvatarResponse }
+
+    public init() { }
+
+    public func encode(to container: GunBoundEncodingContainer) throws {
+        try container.encode(UInt16(0x0000)) // RTC
+    }
+}
