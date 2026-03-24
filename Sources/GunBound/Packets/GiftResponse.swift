42228e08145e3278db06127932837f7677b24b6c
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-23 21:39:02 -0400

    Add GiftResponse (0x6037)

---
+++ Sources/GunBound/Packets/GiftResponse.swift
@@ -0,0 +1,11 @@
+/// Gift Response (0x6037)
+public struct GiftResponse: GunBoundPacket, GunBoundEncodable, Encodable {
+
+    public static var opcode: Opcode { .giftResponse }
+
+    public init() { }
+
+    public func encode(to container: GunBoundEncodingContainer) throws {
+        try container.encode(UInt16(0x6005)) // RTC
+    }
+}
