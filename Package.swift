2c90fccb6b127d835c3638e0b2768ffe138d7faf
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-23 22:43:27 -0400

    Update for Swift 6

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 6.0
 import PackageDescription
 
 let swiftSettings: [SwiftSetting] = [
