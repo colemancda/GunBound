cdce9fa5cdb781f7ba2da9285e86ecee7130a9d1
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-25 08:05:12 -0400

    Fix `UserIdResponse`

--- Sources/GunBound/Packets/UserIdResponse.swift
+++ Sources/GunBound/Packets/UserIdResponse.swift
@@ -11,42 +11,29 @@ import Foundation
 ///
 /// Response containing player information (encrypted)
 public struct UserIdResponse: GunBoundPacket, Codable, Equatable, Hashable {
-    public static var opcode: Opcode { .userResponse }
 
-    public static var isEncrypted: Bool { true }
+    public static var opcode: Opcode { .userResponse }
 
     /// The player's nickname (display name)
     public var nickname1: Username
     /// The player's secondary nickname (display name)
     public var nickname2: Username
     /// The player's guild name
-    public var rankCurrent: Int16
+    public var guild: Guild
     /// The player's rank for current season (points)
-    public var rankSeason: Int16
+    public var rankCurrent: Int16
     /// The player's total games played
-    public var rankSeason: Int16 = 0)
+    public var rankSeason: Int16
+
     public init(
         nickname1: Username,
-        nickname2: Username
+        nickname2: Username,
         guild: Guild = "",
         rankCurrent: Int16 = 0,
         rankSeason: Int16 = 0
     ) {
         self.nickname1 = nickname1
         self.nickname2 = nickname2
-        self.guild = guild
-        self.rankCurrent = rankCurrent
-        self.rankSeason = rankSeason
-    }
-
-    public init(
-        nickname: Username,
-        guild: Guild = "",
-        rankCurrent: Int16 = 0,
-        rankSeason: Int16 = 0
-    ) {
-        self.nickname1 = nickname
-        self.nickname2 = nickname
         self.guild = guild
         self.rankCurrent = rankCurrent
         self.rankSeason = rankSeason
